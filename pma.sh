6aad299fd3ea7d8317bdb92e19fe9b193e20c16a
Lars Gunther <gunther@keryx.se>
2016-08-25 11:00:02 +0200

    latest version + swedish + adapted for my book

--- pma.sh
+++ pma.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-echo 'Downloading phpMyAdmin 4.6.3'
-curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.6.3/phpMyAdmin-4.6.3-english.tar.gz -o phpmyadmin.tar.gz
+# Fixed for my book Webbserverprogrammering 1
 
-mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
+cd ~/webprojects
 
-rm phpmyadmin.tar.gz
+# TODO Always get latest version
+echo 'laddar ner phpMyAdmin 4.6.4'
+curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.6.4/phpMyAdmin-4.6.4-all-languages.tar.xz -o phpmyadmin.tar.xz
 
+echo 'packar upp phpMyAdmin'
+mkdir phpmyadmin && tar xf phpmyadmin.tar.xz -C phpmyadmin --strip-components 1
+
+rm phpmyadmin.tar.xz
+
+echo 'Aktiverar phpMyAdmin som en Nginx virtual host'
 CMD=/vagrant/scripts/serve-laravel.sh
 if [ ! -f $CMD ]; then
     # fallback for older versions
+    # As of 2016-08-25 this includes Homestead Improved
     CMD=/vagrant/scripts/serve.sh
 fi
 
 sudo bash $CMD phpmyadmin.app $(pwd)/phpmyadmin
+sudo nginx -s reload
+
+echo 'Färdig. Lägg till 192.168.10.10 phpmyadmin.app i din hosts-fil'
