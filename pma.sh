3d7eb271b692f4f0046725f0d3d48f6c42fcad90
Lars Gunther <gunther@keryx.se>
2016-08-30 10:44:09 +0200

    Color in echo

--- pma.sh
+++ pma.sh
@@ -5,27 +5,43 @@
 # Detta skript ska köras inuti din virtuella maskin
 if [[ $HOSTNAME != "homestead" ]]
 then
-    echo "Skriptet ska köras inuti din virtuella maskin (homestead)"
+    echo -e "\e[31mSkriptet ska köras inuti din virtuella maskin (homestead)\e[39m"
     exit 1
 fi
 
 cd ~/webprojects
-if [[ $PWD != "webprojects" ]]
+if [[ $PWD != "/home/vagrant/webprojects" ]]
 then
-    echo "Mappen webprojects saknas"
+    echo -e "\e[31mMappen webprojects saknas\e[39m"
     exit 1
 fi
+echo -e "\e[33mInstallerar unzip och php-bz2\e[39m"
+sudo apt-get install -y unzip php-bz2
 
-# TODO Always get latest version
-echo 'Laddar ner phpMyAdmin 4.6.4'
+echo -e "\e[33mLaddar ner phpMyAdmin\e[39m"
+# Inte garanterat senaste versionen, men med alla språk
 curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.6.4/phpMyAdmin-4.6.4-all-languages.tar.xz -o phpmyadmin.tar.xz
-
-echo 'Packar upp phpMyAdmin'
 mkdir phpmyadmin && tar xf phpmyadmin.tar.xz -C phpmyadmin --strip-components 1
-
 rm phpmyadmin.tar.xz
 
-echo 'Aktiverar phpMyAdmin som en Nginx virtual host'
+# Senaste version kan hämtas som nedan, men då finns inte svenska som språk
+#wget https://github.com/phpmyadmin/phpmyadmin/archive/STABLE.zip
+#echo -e "\e[33mPackar upp phpMyAdmin\e[39m"
+#unzip STABLE.zip
+#sudo mv phpmyadmin-STABLE/ phpmyadmin
+#rm STABLE.zip
+
+# Översättning
+echo -e "\e[33mKonfigurerar phpMyAdmin\e[39m"
+cp phpmyadmin/config.sample.inc.php phpmyadmin/config.inc.php
+blowfish=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
+sed -i -e "s@'blowfish_secret'] = ''@'blowfish_secret'] = '$blowfish'@g" phpmyadmin/config.inc.php
+sed -i -e "s@//\$cfg\['DefaultLang'\] = 'de';@\$cfg\['DefaultLang'\] = 'sv';@" phpmyadmin/config.inc.php
+# Windows + Homestead requires this
+echo "\$cfg['CheckConfigurationPermissions'] = false;" >> phpmyadmin/config.inc.php
+
+
+echo -e "\e[33mAktiverar phpMyAdmin som en Nginx virtual host\e[39m"
 CMD=/vagrant/scripts/serve-laravel.sh
 if [ ! -f $CMD ]; then
     # fallback for older versions
@@ -36,4 +52,4 @@ fi
 sudo bash $CMD phpmyadmin.app $(pwd)/phpmyadmin
 sudo nginx -s reload
 
-echo 'Färdig. Lägg till 192.168.10.10 phpmyadmin.app i din värddators hosts-fil'
+echo -e "\e[33mFärdig. Lägg själv till 192.168.10.10 phpmyadmin.app i din värddators hosts-fil\e[39m"
